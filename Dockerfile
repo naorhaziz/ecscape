77eb191e28e0a7ad11f68fb4a57aaf3ec36bd0b0
Naor Haziz <naorh@sweet.security>
2025-03-31 14:55:57 +0300

    change to imds v1

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM alpine:latest
 
-COPY --from=gcr.io/distroless/static-debian12:latest /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
-
 ENV RUST_BACKTRACE=full
 # anyhow creates a backtrace for every error, which can be quite taxing, turning it off for now
 ENV RUST_LIB_BACKTRACE=0
--- src/imds_metadata.rs
+++ src/imds_metadata.rs
@@ -1,22 +1,12 @@
 use anyhow::Result;
-use reqwest::{
-    Client, ClientBuilder, Response,
-    header::{HeaderMap, HeaderValue},
-};
+use reqwest::{Client, Response};
 use serde::Deserialize;
 
 const IMDS_BASE: &str = "http://169.254.169.254";
-
-const IMDS_TOKEN_PATH: &str = "latest/api/token";
 const IMDS_LOCAL_IPV4_PATH: &str = "latest/meta-data/local-ipv4";
 const IMDS_REGION_PATH: &str = "latest/meta-data/placement/region";
 const IMDS_ROLE_NAME_PATH: &str = "latest/meta-data/iam/security-credentials";
-
 const IMDS_ROLE_CREDS_PATH_PREFIX: &str = "latest/meta-data/iam/security-credentials/";
-
-const HEADER_TOKEN_TTL: &str = "X-aws-ec2-metadata-token-ttl-seconds";
-const HEADER_TOKEN_TTL_VALUE: &str = "21600";
-const HEADER_METADATA_TOKEN: &str = "X-aws-ec2-metadata-token";
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -37,7 +27,7 @@ pub struct IMDSMetadata {
 
 impl IMDSMetadata {
     pub async fn try_new() -> Result<Self> {
-        let client = Self::build_client().await?;
+        let client = Client::new();
 
         let local_ip = Self::imds_get(&client, IMDS_LOCAL_IPV4_PATH)
             .await?
@@ -66,34 +56,12 @@ impl IMDSMetadata {
         })
     }
 
-    fn imds_url(path: &str) -> String {
-        format!("{}/{}", IMDS_BASE, path)
-    }
-
     async fn imds_get(client: &Client, path: &str) -> Result<Response> {
         let response = client
-            .get(Self::imds_url(path))
+            .get(format!("{}/{}", IMDS_BASE, path))
             .send()
             .await?
             .error_for_status()?;
         Ok(response)
-    }
-
-    async fn build_client() -> Result<Client> {
-        let token = Client::new()
-            .put(Self::imds_url(IMDS_TOKEN_PATH))
-            .header(HEADER_TOKEN_TTL, HEADER_TOKEN_TTL_VALUE)
-            .send()
-            .await?
-            .error_for_status()?
-            .text()
-            .await?;
-
-        let mut headers = HeaderMap::new();
-        headers.insert(HEADER_METADATA_TOKEN, HeaderValue::from_str(&token)?);
-
-        let client = ClientBuilder::new().default_headers(headers).build()?;
-
-        Ok(client)
     }
 }
